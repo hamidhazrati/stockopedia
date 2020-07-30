aedab87839ff0aa2e12a529960d323a98563e99d
hamidhazrati <hamid.hazrati@hmcts.net>
2020-07-30 17:36:30 +0100

    stoclopedia spelling  commit

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../coverage/stoclopedia'),
+      dir: require('path').join(__dirname, '../coverage/Stockopedia'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
